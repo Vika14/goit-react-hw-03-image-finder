b4b4330173c980abbccf2abe8aaa3ec97ba22729
Vika14 <manunya1406@gmail.com>
2023-09-04 21:45:39 +0200

    hw3-6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,67 @@
 import React, { Component } from 'react';
 import { Loader } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
-import { fetchImages } from './FetchImages/FetchImages';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
+import { fetchImages } from './Services/api';
 
 export class App extends Component {
   state = {
     images: [],
     isLoading: false,
     currentSearch: '',
+    totalImages: 0,
     page: 1,
     modalOpen: false,
     modalImg: '',
     modalAlt: '',
   };
 
+  async componentDidUpdate(_, prevState) {
+    const { page, currentSearch } = this.state;
+    if (prevState.page !== page || prevState.currentSearch !== currentSearch) {
+      this.setState({ isLoading: true });
+
+      try {
+        const { images, totalImages } = await fetchImages(
+          currentSearch,
+          this.state.page
+        );
+        if (totalImages === 0) {
+          alert('No images');
+          return;
+        }
+        this.setState(prevState => {
+          return { images: [...prevState.images, ...images], totalImages };
+        });
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
   handleSubmit = async e => {
     e.preventDefault();
-    this.setState({ isLoading: true });
     const inputSearch = e.target.elements.inputSearch;
     if (inputSearch.value.trim() === '') {
       return;
     }
-    const response = await fetchImages(inputSearch.value, 1);
     this.setState({
-      images: response,
-      isLoading: false,
       currentSearch: inputSearch.value,
       page: 1,
+      images: [],
+      totalImages: 0,
     });
   };
 
   handleLoadMore = async () => {
-    const response = await fetchImages(
-      this.state.currentSearch,
-      this.state.page + 1
-    );
-    this.setState({
-      images: [...this.state.images, ...response],
-      page: this.state.page + 1,
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
     });
   };
 
@@ -67,7 +88,11 @@ export class App extends Component {
   };
 
   async componentDidMount() {
-    window.addEventListener('keydown', this.handleClose);
+    document.addEventListener('keydown', this.handleClose);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleClose);
   }
 
   render() {
